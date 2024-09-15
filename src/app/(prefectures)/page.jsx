a606c008c03c6f0de83dea10dc32669d8460f46b
shimizu-tomoya <shimitomo.pix@gmail.com>
2024-09-15 20:46:27 +0900

    [update]コードの変更

---
+++ src/app/(prefectures)/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <Link href="/chiba">千葉県</Link>
+      東京都
+    </div>
+  );
+}
--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20 grid">
-      千葉県 東京都
-    </div>
-  );
-}
