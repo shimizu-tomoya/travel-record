7ea894b9a2eeaa08b5973900968d29945904d7ac
shimizu-tomoya <shimitomo.pix@gmail.com>
2024-09-15 19:04:42 +0900

    [update]コードの変更

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] grid min-h-screen">
+    <div className="min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20 grid">
       千葉県 東京都
     </div>
   );
